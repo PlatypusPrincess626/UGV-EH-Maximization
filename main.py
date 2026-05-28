3bc1bd8786a521853ff4b01b58009db2b306deb4
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2026-05-28 16:15:03 -0500

    Update

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ for episode in range(1, TOTAL_EPISODES + 1):
 
         # B. Evaluate state and get stochastic action selection from Normal distribution
         model.eval()
-        flat_obs = torch.tensor(local_patch.flatten(), dtype=torch.float32).unsqueeze(0).to(device)
+        flat_obs = torch.tensor(local_patch.flatten(), dtype=torch.float32, device=device).unsqueeze(0)
 
         with torch.no_grad():
             action_mean = model.forward(flat_obs).squeeze(0)
