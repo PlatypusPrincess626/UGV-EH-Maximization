75d62030809f55ca6b54e20407970a7ba4c6a8d4
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2026-07-02 17:01:44 -0500

    Update

--- ugv_simulator.py
+++ ugv_simulator.py
@@ -483,7 +483,7 @@ class UGVSimulator:
                    sol_area, tilt, azimuth):
 
         spectra, solpos = env.get_spectrum(
-            self.x, self.y, tilt, azimuth, step
+            round(self.x), round(self.y), tilt, azimuth, step
         )
 
         sun_azimuth = solpos["azimuth"].iloc[0]
