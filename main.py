485a89bd952b664c216575f1fef699cbe9cd2ed9
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2026-07-11 11:51:59 -0500

    Update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import time
 
 # ============================================================
 # Set POLICY_TYPE = "transformer" or "pso"
-POLICY_TYPE = "transformer"
+POLICY_TYPE = "pso"
 if POLICY_TYPE == "transformer":
     # Set TRANSFORMER_VARIANT = "normal" or "chaotic" or "lyapunov"
     TRANSFORMER_VARIANT = "lyapunov"
