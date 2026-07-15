d258988f712a1ee6cff0859b6299f0d753c0dcc4
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2026-07-14 19:33:27 -0500

    Update

--- chaotic_lyupnov_transformer.py
+++ chaotic_lyupnov_transformer.py
@@ -87,11 +87,11 @@ class ChebyshevLyapunovTransformerActorCritic(nn.Module):
         scalar_dim=7,
         action_dim=2,
         sequence_length=12,
-        d_model=64,  #128
-        energy_dim=32,  #64
+        d_model=16,  #128
+        energy_dim=8,  #64
         nhead=4,
         num_layers=2,
-        dim_feedforward=64,  #256
+        dim_feedforward=16,  #256
         dropout=0.1,
     ):
         super().__init__()
